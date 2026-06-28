501a2ac4b6520885aa019f9ef818c6c1e5bf4b20
Truusje13 <truusje13@users.noreply.github.com>
2026-06-28 11:01:03 +0200

    Fix: nummers-3 deadline naar verleden gezet (sluit per direct)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "wk-poule-v20";
+const CACHE_VERSION = "wk-poule-v21";
 const ASSETS = ["/", "/index.html", "/style.css", "/app.js"];
 
 // Bij installatie: cache vullen en meteen activeren
